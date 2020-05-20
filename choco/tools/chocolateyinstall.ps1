af44a536f986ac2570ca9ee58d97b4786ed4bbf0
Alex Davies <alex@codecutout.com>
2020-05-20 22:12:57 +0100

    Add choco package spec

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = Join-Path $env:APPDATA "terraform.d\plugins"
+    file          = Join-Path $toolsDir 'terraform-plugins.zip'
+  }
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ choco/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+    packageName   = $env:ChocolateyPackageName
+    zipFileName   = "$($env:ChocolateyPackageName)Install.zip"
+}
+
+Uninstall-ChocolateyZipPackage @packageArgs
