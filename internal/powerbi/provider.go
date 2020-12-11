8e237316ed8ba454b4cd3a623d1b65145506080c
U, Vijaykumar <vijaykumar.u@nuance.com>
2020-12-12 01:48:02 +0530

    setting provider parameter "grant_type" to password as default

--- internal/powerbi/provider.go
+++ internal/powerbi/provider.go
@@ -18,7 +18,7 @@ func Provider() *schema.Provider {
 			"grant_type": {
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("POWERBI_GRANT_TYPE", ""),
+				DefaultFunc: schema.EnvDefaultFunc("POWERBI_GRANT_TYPE", "password"),
 				Description: "The grant type for the a Power BI to define token method using username and password or Application id with admin grants. This can also be sourced from the `POWERBI_GRANT_TYPE` Environment Variable",
 			},
 			"client_id": {
